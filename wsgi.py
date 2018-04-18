911668656553ffaad20479e3d00698fe0b228855
Rimcoin <38360606+rimcoin@users.noreply.github.com>
2018-04-18 16:50:01 +0000

    Fix security vulnerability
    Now checks if sender has enough Rimcoin to send.

--- wsgi.py
+++ wsgi.py
@@ -21,6 +21,8 @@ def app(p,env):
     elif command=="send":
         i=open('users','r') # get users file
         i=eval(i.read()) # read said file
+        if (i[path[3]]-float(path[2]))<=0:
+        	return "";
         if eval(open('ids','r').read())[path[3]]==hash(path[4]) and float(path[2])>0: # if the verification number is correct, send the money
             i[path[1]]+=float(path[2]) # give reciever money
             i[path[3]]-=float(path[2]) # remove sender's money
